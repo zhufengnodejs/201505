5ac306e3a789b1d4e52b81a6a8a1dca08637be1a
zhangrenyang-t510 <zhang_renyang@>
2015-08-02 17:59:20 +0800

    init

--- 7.event/7.done3.js
+++ 7.event/7.done3.js
@@ -3,18 +3,19 @@ var EventEmitter = require('events').EventEmitter;
 var user = {};
 var count =0;
 var e = new EventEmitter();
-e.on('name',done);
-e.on('age',done);
-
 var done = function(key,value){
     user[key] = value;
     if(++count==2)
         console.dir(user);
 }
+e.on('name',done);
+e.on('age',done);
+
+
 fs.readFile('./name.txt','utf8',function(err,data){
-    e.emit('name',data);
+    e.emit('name','name',data);
 });
 fs.readFile('./age.txt','utf8',function(err,data){
-    e.emit('age',data);
+    e.emit('age','age',data);
 });
 
