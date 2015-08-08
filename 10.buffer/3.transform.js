97602c1e527484d1e737098ada8462c2b5fbc770
zhangrenyang-t510 <zhang_renyang@>
2015-08-08 11:35:23 +0800

    buffer

---
+++ 10.buffer/3.transform.js
@@ -0,0 +1,11 @@
+
+var a = 0xff;//255
+console.log(parseInt("11",2));//2进制转10进制
+console.log(parseInt("777",8));//8进制转10进制
+console.log(7+7*8+7*8*8);
+
+//把10进制转成2进制 8 16
+console.log((1).toString(16));//1
+//16 41 65
+console.log(parseInt("41",16));
+
