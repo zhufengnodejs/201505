e8a922c249a53af064497808e026dd2e8318a8a7
zhangrenyang-t510 <zhang_renyang@>
2015-08-29 16:17:43 +0800

    shop

---
+++ 22.zhufengshop/zhufengshop/public/javascripts/app.js
@@ -0,0 +1,27 @@
+angular.module('shopApp',['ngRoute'])
+    .config(function($routeProvider,$locationProvider){
+        $routeProvider.when('/',{
+            templateUrl:'pages/home.html',
+            controller:'HomeCtrl'
+        }).when('/reg',{
+            templateUrl:'pages/reg.html',
+            controller:'RegCtrl'
+        }).when('/login',{
+            templateUrl:'pages/login.html',
+            controller:'LoginCtrl'
+        }).otherwise({
+            redirectTo:'/'
+        });
+    });
+
+angular.module('shopApp').controller('HomeCtrl',function(){
+
+});
+
+angular.module('shopApp').controller('RegCtrl',function(){
+
+});
+
+angular.module('shopApp').controller('LoginCtrl',function(){
+
+});
